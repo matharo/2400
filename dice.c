b5e042cdd5ec922b8f6c3fe3ce8a9f84fd5c824b
Lorna Xiao <lornaxiao@hopper.mcs.slu.edu>
2017-02-19 07:55:06 -0600

    editted file

--- dice.c
+++ dice.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[]) 
-//arg[0]=./dice
+#include <stdlib.h>
+int main(int argc, char* argv[]) //arg[0]=./dice
 {
-	int N = strtol(arg[1],NULL,10); //number of throws
-	int D = strtol(arg[2],NULL,10); //number of dice
+	int N = strtol(argv[1],NULL,10); //number of throws
+	int D = strtol(argv[2],NULL,10); //number of dice
 
-	int prob = D*6; //max possible value of throwing D dice
+	int maxval = D*6; //max possible value of throwing D dice
 
-	int sumOfDice[N]; //hold sums of dice per roll
+	int *sumOfDice = malloc(N*sizeof(int)); //hold combos of die per roll, holds N numbers
 
-	//array with length of throwing D dice = D*6
+	//array with length of maxval
 	int *array = malloc(D*6*sizeof(int)); 
 	
-	int i; //argument number
+	int i; 
 	for (i = 0; i < N; i++) //for each throw
 	{
 		int one_dice = 0; //a dice
@@ -27,22 +26,22 @@ int main(int argc, char* argv[])
 		sumOfDice[i] = one_dice; //holds sum of each throw 
 	}
 
-	for (i = 1; i <= prob; i++) //for every possible value
+	for (i = 0; i < maxval; i++) //for every possible max value
 	{
 		int j;
-		for (j = 0; j < N; j++) //for each number in sumOfDice;
+		for (j = 0; j < N; j++) //for each number in sumOfDice, should have N numbers
 		{
-			if (sumOfDice[j] == i) //if that number is one of possible values
-			{
-				array[i] += 1; //count that number
+			if (sumOfDice[j] == (i+1)) //if that number is one of possible values
+			{				//increment i because impossible to get sum of 0
+				array[i] += 1; //count that number and increment
 			}
 		}
 	}
 
-
-	printf("Throwing %d dice %d times\n",
-	for (i = 1; i <= prob; i++)
+	printf("Throwing %d dice %d times\n",D,N);
+	printf("The probability of getting a combination of \n");
+	for (i = 0; i < maxval; i++) //for every possible value
 	{
-		printf(
-
-	
+		printf("%d is: %d/%d\n",i+1,array[i],maxval);
+	}				//increment i, because no possible value of 0
+}
