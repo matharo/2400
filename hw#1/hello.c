56254316ef19a61f471bdc831e6c8c545714cae4
Lorna Xiao <lornaxiao@hopper.mcs.slu.edu>
2017-02-20 17:36:37 -0600

    add folder, move files

---
+++ hw#1/hello.c
@@ -0,0 +1,19 @@
+/*Create a program that does a simple output to the console*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello, World!\n");
+
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("Argument %d is %s\n",i,argv[i]);
+		//%d = an interger will be here
+		//%s = a String will be here, input = String
+		//\n = endline
+	}
+
+	return 0;
+}
